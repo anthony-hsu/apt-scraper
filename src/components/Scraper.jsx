34be31bd8116d1864a3525d8be939f003de26511
Anthony Hsu <129446589+anthony-hsu@users.noreply.github.com>
2024-01-22 07:29:32 -0800

    Add loading screen

--- src/components/Scraper.jsx
+++ src/components/Scraper.jsx
@@ -1,86 +1,119 @@
 import axios from "axios";
 import { useState } from "react";
-import { Button, FormControl, InputLabel, MenuItem, Select, TextField } from "@mui/material";
+import {
+  Button,
+  FormControl,
+  InputLabel,
+  MenuItem,
+  Select,
+  TextField,
+} from "@mui/material";
 
 const ScraperComponent = () => {
   // States
-  const [neighborhood, setNeighborhood] = useState("");
+  const [neighborhood, setNeighborhood] = useState("Capitol Hill");
   const [city, setCity] = useState("Seattle");
   const [state, setState] = useState("WA");
   const [beds, setBeds] = useState(3);
-  const [maxPrice, setMaxPrice] = useState("");
-  const [minSqft, setMinSqft] = useState("");
+  const [maxPrice, setMaxPrice] = useState("2500");
+  const [minSqft, setMinSqft] = useState("700");
+  const [aptData, setAptData] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
 
   const handleScrape = async () => {
+    setIsLoading(true);
     try {
-      axios.get(`http://127.0.0.1:5000/scrape/${neighborhood}/${city}/${state}/${beds}/${maxPrice}/${minSqft}`).then((response) => {
-        alert("Scraping finished!");
-        console.log(response.data);
-      });
+      axios
+        .get(
+          `http://127.0.0.1:5000/scrape/${neighborhood}/${city}/${state}/${beds}/${maxPrice}/${minSqft}`
+        )
+        .then((response) => {
+          console.log(response.data);
+          setAptData(response.data);
+          setIsLoading(false);
+        });
     } catch (error) {
       console.error("Error scraping:", error);
     }
   };
 
   return (
-    <div className="scraper-container">
-      <h2>Apartment Scraper</h2>
-      <TextField
-        id="input-neighborhood"
-        label="Neighborhood"
-        variant="outlined"
-        value={neighborhood}
-        onChange={(e) => setNeighborhood(e.target.value)}
-      />
-      <TextField
-        id="input-city"
-        label="City"
-        variant="outlined"
-        value={city}
-        onChange={(e) => setCity(e.target.value)}
-      />
-      <TextField
-        id="input-state"
-        label="State"
-        variant="outlined"
-        value={state}
-        onChange={(e) => setState(e.target.value)}
-      />
-      <FormControl fullWidth>
-        <InputLabel id="beds-label">Bedrooms</InputLabel>
-        <Select
-          labelId="beds-label"
-          id="beds-select"
-          value={beds}
-          label="Bedrooms"
-          onChange={(e) => setBeds(e.target.value)}
-        >
-          <MenuItem value={"0"}>Studio</MenuItem>
-          <MenuItem value={"1"}>One</MenuItem>
-          <MenuItem value={"2"}>Two</MenuItem>
-          <MenuItem value={"3"}>All</MenuItem>
-        </Select>
-      </FormControl>
-      <TextField
-        id="input-price-max"
-        label="Price (Max)"
-        variant="outlined"
-        type="number"
-        value={maxPrice}
-        onChange={(e) => setMaxPrice(e.target.value)}
-      />
-      <TextField
-        id="input-sqft-min"
-        label="Square Feet (Min)"
-        variant="outlined"
-        type="number"
-        value={minSqft}
-        onChange={(e) => setMinSqft(e.target.value)}
-      />
-      <Button variant="contained" onClick={handleScrape}>
-        Scrape Data
-      </Button>
-    </div>
+    <>
+      <h1>Apartment Scraper</h1>
+
+      {isLoading ? (
+        <div>Loading...</div>
+      ) : (
+        <>
+          {aptData?.length > 0 ? (
+            <div>Data Loaded!</div>
+          ) : (
+            <div className="scraper-container">
+              <TextField
+                id="input-neighborhood"
+                label="Neighborhood"
+                variant="outlined"
+                value={neighborhood}
+                onChange={(e) => setNeighborhood(e.target.value)}
+              />
+              <TextField
+                id="input-city"
+                label="City"
+                variant="outlined"
+                value={city}
+                onChange={(e) => setCity(e.target.value)}
+              />
+              <TextField
+                id="input-state"
+                label="State"
+                variant="outlined"
+                value={state}
+                onChange={(e) => setState(e.target.value)}
+              />
+              <FormControl fullWidth>
+                <InputLabel id="beds-label">Bedrooms</InputLabel>
+                <Select
+                  labelId="beds-label"
+                  id="beds-select"
+                  value={beds}
+                  label="Bedrooms"
+                  onChange={(e) => setBeds(e.target.value)}
+                >
+                  <MenuItem value={"0"}>Studio</MenuItem>
+                  <MenuItem value={"1"}>One</MenuItem>
+                  <MenuItem value={"2"}>Two</MenuItem>
+                  <MenuItem value={"3"}>All</MenuItem>
+                </Select>
+              </FormControl>
+              <TextField
+                id="input-price-max"
+                label="Price (Max)"
+                variant="outlined"
+                type="number"
+                value={maxPrice}
+                onChange={(e) => setMaxPrice(e.target.value)}
+              />
+              <TextField
+                id="input-sqft-min"
+                label="Square Feet (Min)"
+                variant="outlined"
+                type="number"
+                value={minSqft}
+                onChange={(e) => setMinSqft(e.target.value)}
+              />
+              <Button
+                variant="contained"
+                onClick={() => {
+                  handleScrape();
+                }}
+              >
+                Scrape Data
+              </Button>
+            </div>
+          )}
+        </>
+      )}
+    </>
   );
 };
 
